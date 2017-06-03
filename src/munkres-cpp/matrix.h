56cdf09fcffe2f18d4e65962dc2b204c809c61ac
Gluttton <gluttton@ukr.net>
2017-06-03 19:27:34 +0300

    Reduce code: remove logic which are already implemented in base class; use another memory allocation policy; remove unuded functions; etc.

--- src/munkres-cpp/matrix.h
+++ src/munkres-cpp/matrix.h
@@ -34,23 +34,28 @@ template<class T>
 class Matrix : public matrix_base<T>
 {
     public:
-        Matrix ();
-        Matrix (const size_t rows, const size_t columns);
-        Matrix (const std::initializer_list<std::initializer_list<T>> & init);
-        Matrix (const Matrix<T> & other);
-        Matrix<T> & operator= (const Matrix<T> & other);
+        Matrix () = default;
+        Matrix (const size_t, const size_t);
+        Matrix (const std::initializer_list<std::initializer_list<T>> &);
+        Matrix (const Matrix<T> &);
+        Matrix<T> & operator= (const Matrix<T> &);
         ~Matrix () override;
         // All operations modify the matrix in-place.
-        void resize (const size_t rows, const size_t columns, const T default_value = 0);
-        void clear ();
-        const T & operator () (const size_t x, const size_t y) const override;
-        T & operator () (const size_t x, const size_t y) override;
-        T min () const;
-        T max () const;
-        inline size_t minsize ()
+        void resize (const size_t, const size_t, const T default_value = T (0) ) override;
+        const T & operator () (const size_t x, const size_t y) const override
         {
-            return ( (m_rows < m_columns) ? m_rows : m_columns);
-        }
+            assert (x < m_rows);
+            assert (y < m_columns);
+            assert (m_matrix != nullptr);
+            return m_matrix [x][y];
+        };
+        T & operator () (const size_t x, const size_t y) override
+        {
+            assert (x < m_rows);
+            assert (y < m_columns);
+            assert (m_matrix != nullptr);
+            return m_matrix[x][y];
+        };
         size_t columns () const override
         {
             return m_columns;
@@ -61,32 +66,19 @@ class Matrix : public matrix_base<T>
         }
 
     private:
-        T ** m_matrix;
-        size_t m_rows;
-        size_t m_columns;
+        T ** m_matrix    {nullptr};
+        size_t m_rows    {0};
+        size_t m_columns {0};
 };
 
 
 
 template<class T>
-Matrix<T>::Matrix ()
-    : m_matrix {nullptr}
-    , m_rows {0}
-    , m_columns {0}
-{
-    assert (!(m_matrix && m_rows && m_columns) );
-}
-
-
-
-template<class T>
 Matrix<T>::Matrix (const std::initializer_list<std::initializer_list<T>> & init)
-    : Matrix<T>()
+    : Matrix<T> ()
 {
     m_rows = init.size ();
-    if (m_rows == 0) {
-        m_columns = 0;
-    } else {
+    if (m_rows) {
         m_columns = init.begin ()->size ();
         if (m_columns > 0) {
             resize (m_rows, m_columns);
@@ -107,20 +99,11 @@ Matrix<T>::Matrix (const std::initializer_list<std::initializer_list<T>> & init)
 
 template<class T>
 Matrix<T>::Matrix (const Matrix<T> & other)
+    : Matrix<T> ()
 {
-    if (other.m_matrix != nullptr) {
-        // Copy arrays.
-        m_matrix = nullptr;
+    if (other.m_matrix) {
         resize (other.m_rows, other.m_columns);
-        for (size_t j = 0; j < m_columns; j++) {
-            for (size_t i = 0; i < m_rows; i++) {
-                m_matrix[i][j] = other.m_matrix[i][j];
-            }
-        }
-    } else {
-        m_matrix = nullptr;
-        m_rows = 0;
-        m_columns = 0;
+        std::copy (other.m_matrix[0], other.m_matrix[0] + m_rows * m_columns, m_matrix[0]);
     }
 }
 
@@ -128,7 +111,7 @@ Matrix<T>::Matrix (const Matrix<T> & other)
 
 template<class T>
 Matrix<T>::Matrix (const size_t rows, const size_t columns)
-    : m_matrix {nullptr}
+    : Matrix<T> ()
 {
     resize (rows, columns);
 }
@@ -139,19 +122,11 @@ template<class T>
 Matrix<T> & Matrix<T>::operator= (const Matrix<T> & other)
 {
     if (other.m_matrix != nullptr) {
-        // Copy arrays.
         resize (other.m_rows, other.m_columns);
-        for (size_t j = 0; j < m_columns; j++) {
-            for (size_t i = 0; i < m_rows; i++) {
-                m_matrix[i][j] = other.m_matrix[i][j];
-            }
-        }
+        std::copy (other.m_matrix[0], other.m_matrix[0] + m_rows * m_columns, m_matrix[0]);
     } else {
         // Free arrays.
-        for (size_t i = 0; i < m_columns; i++) {
-            delete [] m_matrix[i];
-        }
-
+        delete [] m_matrix[0];
         delete [] m_matrix;
 
         m_matrix = nullptr;
@@ -168,16 +143,10 @@ template<class T>
 Matrix<T>::~Matrix ()
 {
     if (m_matrix != nullptr) {
-        // Free arrays.
-        for (size_t i = 0; i < m_rows; i++) {
-            delete [] m_matrix[i];
-        }
-
+        delete [] m_matrix[0];
         delete [] m_matrix;
     }
     m_matrix = nullptr;
-
-    assert (m_matrix == nullptr);
 }
 
 
@@ -188,28 +157,17 @@ void Matrix<T>::resize (const size_t rows, const size_t columns, const T default
     assert (rows > 0);
     assert (columns > 0);
 
-    if (m_matrix == nullptr) {
-        // Alloc arrays.
-        m_matrix = new T *[rows];           // Rows.
-        for (size_t i = 0; i < rows; i++) {
-            m_matrix[i] = new T[columns];   // Columns.
-        }
+    // Save array pointer.
+    T ** new_matrix;
+    // Alloc new arrays.
+    new_matrix = new T *[rows];             // Rows.
+    new_matrix[0] = new T[rows * columns];  // Columns.
+    for (size_t i = 1; i < rows; i++) {
+        new_matrix[i] = new_matrix[0] + i * columns;
+    }
+    std::fill (new_matrix[0], new_matrix[0] + rows * columns, default_value);
 
-        m_rows = rows;
-        m_columns = columns;
-        clear ();
-    } else {
-        // Save array pointer.
-        T ** new_matrix;
-        // Alloc new arrays.
-        new_matrix = new T *[rows];         // Rows.
-        for (size_t i = 0; i < rows; i++) {
-            new_matrix[i] = new T[columns]; // Columns.
-            for (size_t j = 0; j < columns; j++) {
-                new_matrix[i][j] = default_value;
-            }
-        }
-
+    if (m_matrix) {
         // Copy data from saved pointer to new arrays.
         const size_t minrows = std::min (rows, m_rows);
         const size_t mincols = std::min (columns, m_columns);
@@ -221,91 +179,14 @@ void Matrix<T>::resize (const size_t rows, const size_t columns, const T default
 
         // Delete old arrays.
         if (m_matrix != nullptr) {
-            for (size_t i = 0; i < m_rows; i++) {
-                delete [] m_matrix[i];
-            }
-
+            delete [] m_matrix[0];
             delete [] m_matrix;
         }
-
-        m_rows = rows;
-        m_columns = columns;
-        m_matrix = new_matrix;
-    }
-}
-
-
-
-template<class T>
-void Matrix<T>::clear ()
-{
-    assert (m_matrix != nullptr);
-
-    for (size_t j = 0; j < m_columns; j++) {
-        for (size_t i = 0; i < m_rows; i++) {
-            m_matrix [i][j] = 0;
-        }
-    }
-}
-
-
-
-template<class T>
-inline T & Matrix<T>::operator () (const size_t x, const size_t y)
-{
-    assert (x < m_rows);
-    assert (y < m_columns);
-    assert (m_matrix != nullptr);
-
-    return m_matrix[x][y];
-}
-
-
-
-template<class T>
-inline const T & Matrix<T>::operator () (const size_t x, const size_t y) const
-{
-    assert (x < m_rows);
-    assert (y < m_columns);
-    assert (m_matrix != nullptr);
-
-    return m_matrix [x][y];
-}
-
-
-
-template<class T>
-T Matrix<T>::min () const
-{
-    assert (m_matrix && m_rows && m_columns);
-
-    T min = m_matrix [0][0];
-
-    for (size_t j = 0; j < m_columns; j++) {
-        for (size_t i = 0; i < m_rows; i++) {
-            min = std::min<T>(min, m_matrix [i][j]);
-        }
     }
 
-    return min;
-}
-
-
-
-template<class T>
-T Matrix<T>::max () const
-{
-    assert (m_matrix && m_rows && m_columns);
-
-    T max = m_matrix [0][0];
-
-    for (size_t j = 0; j < m_columns; j++) {
-        for (size_t i = 0; i < m_rows; i++) {
-            max = std::max<T>(max, m_matrix [i][j]);
-        }
-    }
-
-    return max;
+    m_rows = rows;
+    m_columns = columns;
+    m_matrix = new_matrix;
 }
 
 }// namespace munkres_cpp
--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -117,50 +117,6 @@ TEST_F (MatrixTest, resize_From2x2To4x4WithDefaultValueExplicit_Success)
 
 
 
-TEST_F (MatrixTest, clear_1x1_Success)
-{
-    // Arrange.
-    munkres_cpp::Matrix<double> etalon_matrix {
-        {0.0}
-    };
-    munkres_cpp::Matrix<double> test_matrix {
-        {7.0}
-    };
-
-    // Act.
-    test_matrix.clear ();
-
-    // Assert.
-    EXPECT_EQ (etalon_matrix, test_matrix);
-}
-
-
-
-TEST_F (MatrixTest, clear_4x4_Success)
-{
-    // Arrange.
-    munkres_cpp::Matrix<double> etalon_matrix {
-        {0.0, 0.0, 0.0, 0.0},
-        {0.0, 0.0, 0.0, 0.0},
-        {0.0, 0.0, 0.0, 0.0},
-        {0.0, 0.0, 0.0, 0.0}
-    };
-    munkres_cpp::Matrix<double> test_matrix {
-        {1.1, 1.2, 1.3, 1.4},
-        {2.1, 2.2, 2.3, 2.4},
-        {3.1, 3.2, 3.3, 3.4},
-        {4.1, 4.2, 4.3, 4.4}
-    };
-
-    // Act.
-    test_matrix.clear ();
-
-    // Assert.
-    EXPECT_EQ (etalon_matrix, test_matrix);
-}
-
-
-
 TEST_F (MatrixTest, operatorAssignment_0x0_Success)
 {
     // Arrange.
